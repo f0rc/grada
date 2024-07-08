b7432c3541f03c20b0e4453ca556fd0e0182dde4
f0rc <80628768+f0rc@users.noreply.github.com>
2024-07-08 15:55:26 -0500

    url

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -5,6 +5,8 @@
 
   let storageItem = writable<Record<string, any>>({});
 
+  let thisMonth = new Date().toLocaleString("default", { month: "long" });
+
   browser.storage.onChanged.addListener(async (changes) => {
     console.log(changes);
     const newStorageItem = await browser.storage.local.get();
@@ -60,7 +62,9 @@
 <div
   class="flex flex-col bg-neutral-700 text-zinc-300 min-h-[400px] min-w-[400px] p-12"
 >
-  <h1 class="font-semibold uppercase text-center">URL Timer</h1>
+  <h1 class="font-semibold uppercase text-center">
+    {thisMonth}
+  </h1>
   {#each sortedKeys as item}
     <div
       class="flex flex-row justify-between items-center hover:bg-neutral-600 rounded-md px-2 py-2"
@@ -73,3 +77,26 @@
     </div>
   {/each}
 </div>
+<a
+  href="https://github.com/f0rc/grada"
+  class="absolute text-xs bottom-0 right-0 text-zinc-500 p-2"
+  ><div class="flex gap-1 items-center justify-center">
+    <h1>URL Timer</h1>
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-3"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+        />
+      </svg>
+    </div>
+  </div>
+</a>
